39193db5b5f320fc8cef7fdc849ad7236822263e
2121050486 <2121050486@student.humg.edu.vn>
2025-01-06 23:21:44 +0700

    baithuchanh

--- obj/Debug/net8.0/NguyenXuanBao_PTPMQL.AssemblyInfo.cs
+++ obj/Debug/net8.0/NguyenXuanBao_PTPMQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NguyenXuanBao_PTPMQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb2ae8d334a789e8adae0598b081a191161ea6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a547963fc86559e3b9b55318b8fc4f388ad05bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NguyenXuanBao_PTPMQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NguyenXuanBao_PTPMQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
