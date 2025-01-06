61b644ac48d8322f8d3524e284b6fd61c8c0a895
2121050486 <2121050486@student.humg.edu.vn>
2025-01-06 21:34:18 +0700

    baithuchanh

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NGUYENXUANBAO_PTPMQL.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
